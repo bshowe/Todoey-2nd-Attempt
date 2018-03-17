5c6f4883a2fa4c3e96679325142955c9bce5c32f
Benjamin Howe <benjaminhowe@benjamins-macbook-pro.local>
2018-03-17 16:46:29 -0700

    Converted app tp MVC.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Benjamin Howe on 3/17/18.
+//  Copyright © 2018 Benjamin Howe. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
